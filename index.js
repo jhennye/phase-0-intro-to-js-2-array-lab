7ec0f30ebb20fd1b26cc6b53186dae26180716c8
Jhennye Lee <jhennyelee@gmail.com>
2022-11-15 15:09:46 -0500

    commit tree 3b17b7f435633a02f0fcf7c04b47806d852a0d78

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution here!
+  const cats = ["Milo", "Otis", "Garfield"];
+
+
+  function destructivelyAppendCat(name) {
+    cats.push(name)
+  } ;
+  function destructivelyPrependCat(name) {
+    cats.unshift(name)
+  } ;
+
+  function destructivelyRemoveLastCat(name) {
+    cats.pop(name);
+  }
+  function destructivelyRemoveFirstCat(name){
+  cats.shift(name);
+  }
+
+  function appendCat(name) {
+    const oneCat = [...cats, name];
+    return oneCat
+  }
+  function prependCat(name) {
+    const oneCat = [name, ...cats]
+    return oneCat
+  }
+  function removeLastCat(name) {
+    cats.unshift();
+    const copyCats = cats.slice();
+    return cats
+  }
